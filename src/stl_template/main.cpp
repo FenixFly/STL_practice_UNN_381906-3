fdf4be2b0b9ffa445fb4d7b71d3f845feaf745e5
KamenevIlya <ilia.kamenevv@yandex.ru>
2020-12-12 13:41:56 +0300

    Deque

--- src/stl_template/main.cpp
+++ src/stl_template/main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <deque>
+#include <algorithm>
+
+using namespace std;
+
 
 int main()
 {
+    deque<int>test_deque;
+    for (int i = 0; i < 10; i++)
+    {
+        test_deque.push_back(i*5); //добавляем в дек 10 элементов
+    }
+    test_deque.push_front(10);
    
+       for (auto el : test_deque)
+       {
+           cout << el << " "; //вывод дека
+       }
+       cout << endl;
+
+
+       sort(test_deque.begin(), test_deque.end(), [](int a, int b) {return a > b; }); //отсортировать дек по убыванию
+
+    test_deque.pop_back(); //удалить последний элемент
+    for (auto el : test_deque)
+    {
+        cout << el << " "; //вывод дека
+    }
+
+
+
+
     return 0;
 }

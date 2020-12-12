2c5ef018a6e1f7f3883fa188217be1914232367e
feoandrew <33204049+feoandrew@users.noreply.github.com>
2020-12-12 13:49:25 +0300

    comments

--- src/auto_ptr/main.cpp
+++ src/auto_ptr/main.cpp
@@ -1,12 +1,16 @@
 #include<iostream>
 #include<memory>
+#include<locale>
 using namespace std;
+//Феоктистов
+//работа класса auto_ptr
 int main()
 {
-	auto_ptr<int> a(new int), b;
-	*a.get() = 123;
-	cout << "**a.get() = " << *a.get() << endl;
-	b = a;
+
+	auto_ptr<int> a(new int), b; //cоздание "указателей".
+	*a.get() = 123; //a.get() возвращает "нормальный" указатель на данные.
+	cout << "*a.get() = " << *a.get() << endl;
+	b = a;//после операции присваивания указатель a.get() принимает значение NULL.
 	cout << "The assigment b = a has been executed. \n";
 	if (a.get() == NULL)
 		cout << "As a result a.get() is NULL. \n";

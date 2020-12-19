cce771e1e0bab50fc4b0d441b1b33af7e1174474
KamenevIlya <60921799+KamenevIlya@users.noreply.github.com>
2020-12-19 14:27:47 +0300

    Add files via upload

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/iliak/OneDrive/Рабочий стол/STL_practice_UNN_381906-3/src
+# Install script for directory: C:/Users/iliak/OneDrive/Рабочий стол/STL_practice_2/src
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/STL_samples")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/replace_if")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -32,12 +32,6 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for each subdirectory.
-  include("C:/Users/iliak/OneDrive/Рабочий стол/STL_practice_UNN_381906-3/stl_template/cmake_install.cmake")
-
-endif()
-
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -46,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/iliak/OneDrive/Рабочий стол/STL_practice_UNN_381906-3/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/iliak/OneDrive/Рабочий стол/STL_practice_2/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
